0989848a86165bd566df79fc4dcf58421d9ccdf2
Artem Volkov <tirydarten@mail.ru>
2024-11-07 15:34:24 +0700

    code correction overwrite masks.py

--- Home_work_poetry/src/masks.py
+++ Home_work_poetry/src/masks.py
@@ -1,10 +1,29 @@
-def get_mask_card_number(number: str) -> str:
+from typing import Any
+
+
+def get_mask_card_number(number: Any) -> str:
     """Функция принимает строку и возвращает маски карты"""
-    new_string_card = f"{number[0:4]} {number[4:6]}** **** {number[12:]}"
-    return new_string_card
+    for arg in [number]:
+        if not isinstance(arg, str):
+            return "Ошибка типа данных"
+    try:
+        if (number[-16:].isdigit() and len(number) >= 21 and len(number.split()[-1]) == 16) is True:
+            new_string_card = f"** **** {number[-4:]}"
+        return new_string_card
+
+    except UnboundLocalError:
+        return "Неверный формат номера карты"
 
 
-def get_mask_account(number: str) -> str:
+def get_mask_account(number: Any) -> str:
     """Функция принимает строку и возвращает маски счёта"""
-    new_string_account = f"**{number[-4:]}"
-    return new_string_account
+    for arg in [number]:
+        if not isinstance(arg, str):
+            return "Ошибка типа данных"
+    if (
+        number[-20:-1].isdigit() and len(number) == 25 and number[:-21].isalpha() and len(number.split()[-1]) == 20
+    ) is True:
+        new_string_account = f"**{number[-4:]}"
+        return new_string_account
+    else:
+        return "Ошибка в номере счета карты"
