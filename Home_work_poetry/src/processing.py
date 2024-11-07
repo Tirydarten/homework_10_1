604c8ac459b2a2a9787ab726076f47484c50941e
Artem Volkov <tirydarten@mail.ru>
2024-11-07 15:46:17 +0700

    correcting the code and rewriting the file processing.py

--- Home_work_poetry/src/processing.py
+++ Home_work_poetry/src/processing.py
@@ -1,11 +1,16 @@
+from datetime import datetime
+
+
 def filter_by_state(dict_for_search: list, state: str = "EXECUTED") -> list:
     """Функция принимает список словарей и опционально значение для ключа state ('по умолчанию'EXECUTED') и
-    возвращает новый список словарей,у которых ключ state соответствует указанному значению"""
+    возвращает новый список словарей, у которых ключ state соответствует указанному значению"""
     new_dicts = []
     for i in range(len(dict_for_search)):
         if dict_for_search[i]["state"] == state:
             new_dicts.append(dict_for_search[i])
-    return new_dicts
+    if new_dicts:
+        return new_dicts
+    return []
 
 
 my_dict = [
@@ -19,12 +24,17 @@ my_dict = [
 results = filter_by_state(my_dict)
 
 
-def sort_by_date(new_dicts: list, sort_reverse: bool = True) -> list:
+def sort_by_date(new_dicts_sort: list, sort_reverse: bool = True) -> list | str:
     """Функция принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — сортировка по убыванию, т. е. сначала самые последние операции)"""
-    sorted_date = sorted(new_dicts, key=lambda new_dict: new_dict["date"], reverse=sort_reverse)
+    try:
+        for i in range(len(new_dicts_sort)):
+            datetime.strptime(new_dicts_sort[i]["date"], "%Y-%m-%dT%H:%M:%S.%f")
+    except ValueError:
+        return "Неверный формат даты"
+
+    sorted_date = sorted(new_dicts_sort, key=lambda new_dict: new_dict["date"], reverse=sort_reverse)
     return sorted_date
 
 
 fix_result = sort_by_date(results)
-print(f"отсортировано по 'state' и 'date', {fix_result}")
