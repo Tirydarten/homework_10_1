fbd6d23313b926493e3780ad28edb11ebcdbfcb8
Artem Volkov <tirydarten@mail.ru>
2024-11-07 18:36:04 +0700

    committing files after flake8, mypy, black, isort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from src.widget import mask_account_card, get_date
 from src.processing import filter_by_state, sort_by_date
-
+from src.widget import get_date, mask_account_card
 
 print(mask_account_card("Visa Gold 5999414228426353"))
 
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,7 +2,7 @@ from typing import Any
 
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 """Тестирование модуль masks. Функция get_mask_card_number"""
 
