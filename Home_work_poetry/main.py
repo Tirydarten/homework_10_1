d1b607cf452fc020d122a92667f90b1843787245
Artem Volkov <tirydarten@mail.ru>
2024-11-07 16:34:51 +0700

    correcting the code and rewriting the file processing.py and main.py

--- Home_work_poetry/main.py
+++ Home_work_poetry/main.py
@@ -1,18 +1,25 @@
-import src.processing
-from src.widget import get_new_data, mask_account_card
+from Home_work_poetry.src.widget import mask_account_card, get_date
+from Home_work_poetry.src.processing import filter_by_state, sort_by_date
 
 print(mask_account_card("Visa Gold 5999414228426353"))
 
 print(mask_account_card("Счет 73654108430135874305"))
 
-print(get_new_data("2018-07-11T02:26:18.671407"))
+print(get_date("2018-07-11T02:26:18.671407"))
 
-src.processing.sort_by_date(
-    [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 41428829, "state": "EXECUTED", "date": "2020-05-02T18:35:29.512364"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]
-)
+my_dict = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 41428829, "state": "EXECUTED", "date": "2020-05-02T18:35:29.512364"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+results = filter_by_state(my_dict)
+
+fix_result = sort_by_date(results)
+
+if fix_result != "Неверный формат даты":
+    print(f"отсортировано по 'state' и 'date', {fix_result}")
+else:
+    print(fix_result)
--- Home_work_poetry/src/processing.py
+++ Home_work_poetry/src/processing.py
@@ -13,17 +13,6 @@ def filter_by_state(dict_for_search: list, state: str = "EXECUTED") -> list:
     return []
 
 
-my_dict = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 41428829, "state": "EXECUTED", "date": "2020-05-02T18:35:29.512364"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-results = filter_by_state(my_dict)
-
-
 def sort_by_date(new_dicts_sort: list, sort_reverse: bool = True) -> list | str:
     """Функция принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — сортировка по убыванию, т. е. сначала самые последние операции)"""
@@ -35,6 +24,3 @@ def sort_by_date(new_dicts_sort: list, sort_reverse: bool = True) -> list | str:
 
     sorted_date = sorted(new_dicts_sort, key=lambda new_dict: new_dict["date"], reverse=sort_reverse)
     return sorted_date
-
-
-fix_result = sort_by_date(results)
